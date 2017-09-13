107ef26176d0b8a26f29c83398c0fc0e59d8a99c
Kiran Challa <kichalla@microsoft.com>
2017-09-12 17:56:29 -0700

    Added RequestFormLimits filter

---
+++ src/Microsoft.AspNetCore.Mvc.Core/Internal/RequestFormLimitsFilter.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Mvc.Internal
+{
+    /// <summary>
+    /// A filter that sets various limits to the <see cref="HttpRequest.Form"/>
+    /// </summary>
+    public class RequestFormLimitsFilter : IAuthorizationFilter, IRequestFormLimitsPolicy
+    {
+        private readonly ILogger _logger;
+
+        public RequestFormLimitsFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<RequestFormLimitsFilter>();
+        }
+
+        public FormOptions FormOptions { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (IsClosestPolicy(context.Filters))
+            {
+                var features = context.HttpContext.Features;
+                var formFeature = features.Get<IFormFeature>();
+
+                if (formFeature == null || formFeature.Form == null)
+                {
+                    // Request form has not been read yet, so set the limits
+                    features.Set<IFormFeature>(new FormFeature(context.HttpContext.Request, FormOptions));
+                    _logger.AppliedRequestFormLimits();
+                }
+                else
+                {
+                    _logger.CannotApplyRequestFormLimits();
+                }
+            }
+        }
+
+        private bool IsClosestPolicy(IList<IFilterMetadata> filters)
+        {
+            // Determine if this instance is the 'effective' request form limits policy.
+            for (var i = filters.Count - 1; i >= 0; i--)
+            {
+                var filter = filters[i];
+                if (filter is IRequestFormLimitsPolicy)
+                {
+                    return ReferenceEquals(this, filter);
+                }
+            }
+
+            Debug.Fail("The current instance should be in the list of filters.");
+            return false;
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Mvc.Core.Test/RequestFormLimitsAttributeTest.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http.Features;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public class RequestFormLimitsAttributeTest
+    {
+        [Fact]
+        public void CreatesFormOptions_WithDefaults()
+        {
+            // Arrange
+            var formOptions = new FormOptions();
+
+            // Act
+            var requestFormLimitsAttribute = new RequestFormLimitsAttribute();
+
+            // Assert
+            Assert.Equal(formOptions.BufferBody, requestFormLimitsAttribute.BufferBody);
+            Assert.Equal(formOptions.BufferBodyLengthLimit, requestFormLimitsAttribute.BufferBodyLengthLimit);
+            Assert.Equal(formOptions.KeyLengthLimit, requestFormLimitsAttribute.KeyLengthLimit);
+            Assert.Equal(formOptions.MemoryBufferThreshold, requestFormLimitsAttribute.MemoryBufferThreshold);
+            Assert.Equal(formOptions.MultipartBodyLengthLimit, requestFormLimitsAttribute.MultipartBodyLengthLimit);
+            Assert.Equal(formOptions.MultipartBoundaryLengthLimit, requestFormLimitsAttribute.MultipartBoundaryLengthLimit);
+            Assert.Equal(formOptions.MultipartHeadersCountLimit, requestFormLimitsAttribute.MultipartHeadersCountLimit);
+            Assert.Equal(formOptions.MultipartHeadersLengthLimit, requestFormLimitsAttribute.MultipartHeadersLengthLimit);
+            Assert.Equal(formOptions.ValueCountLimit, requestFormLimitsAttribute.ValueCountLimit);
+            Assert.Equal(formOptions.ValueLengthLimit, requestFormLimitsAttribute.ValueLengthLimit);
+        }
+
+        [Fact]
+        public void UpdatesFormOptions_WithOverridenValues()
+        {
+            // Arrange
+            var formOptions = new FormOptions();
+            formOptions.BufferBody = true;
+            formOptions.BufferBodyLengthLimit = 0;
+            formOptions.KeyLengthLimit = 0;
+            formOptions.MemoryBufferThreshold = 0;
+            formOptions.MultipartBodyLengthLimit = 0;
+            formOptions.MultipartBoundaryLengthLimit = 0;
+            formOptions.MultipartHeadersCountLimit = 0;
+            formOptions.MultipartHeadersLengthLimit = 0;
+            formOptions.ValueCountLimit = 0;
+            formOptions.ValueLengthLimit = 0;
+            var requestFormLimitsAttribute = new RequestFormLimitsAttribute();
+
+            // Act
+            requestFormLimitsAttribute.BufferBody = true;
+            requestFormLimitsAttribute.BufferBodyLengthLimit = 0;
+            requestFormLimitsAttribute.KeyLengthLimit = 0;
+            requestFormLimitsAttribute.MemoryBufferThreshold = 0;
+            requestFormLimitsAttribute.MultipartBodyLengthLimit = 0;
+            requestFormLimitsAttribute.MultipartBoundaryLengthLimit = 0;
+            requestFormLimitsAttribute.MultipartHeadersCountLimit = 0;
+            requestFormLimitsAttribute.MultipartHeadersLengthLimit = 0;
+            requestFormLimitsAttribute.ValueCountLimit = 0;
+            requestFormLimitsAttribute.ValueLengthLimit = 0;
+
+            // Assert
+            Assert.Equal(formOptions.BufferBody, requestFormLimitsAttribute.BufferBody);
+            Assert.Equal(formOptions.BufferBodyLengthLimit, requestFormLimitsAttribute.BufferBodyLengthLimit);
+            Assert.Equal(formOptions.KeyLengthLimit, requestFormLimitsAttribute.KeyLengthLimit);
+            Assert.Equal(formOptions.MemoryBufferThreshold, requestFormLimitsAttribute.MemoryBufferThreshold);
+            Assert.Equal(formOptions.MultipartBodyLengthLimit, requestFormLimitsAttribute.MultipartBodyLengthLimit);
+            Assert.Equal(formOptions.MultipartBoundaryLengthLimit, requestFormLimitsAttribute.MultipartBoundaryLengthLimit);
+            Assert.Equal(formOptions.MultipartHeadersCountLimit, requestFormLimitsAttribute.MultipartHeadersCountLimit);
+            Assert.Equal(formOptions.MultipartHeadersLengthLimit, requestFormLimitsAttribute.MultipartHeadersLengthLimit);
+            Assert.Equal(formOptions.ValueCountLimit, requestFormLimitsAttribute.ValueCountLimit);
+            Assert.Equal(formOptions.ValueLengthLimit, requestFormLimitsAttribute.ValueLengthLimit);
+        }
+    }
+}
