67bcdd5c436840be95f352b48b4a27569b5460f6
Kiran Challa <kichalla@microsoft.com>
2017-09-15 17:37:01 -0700

    updated

---
+++ src/Microsoft.AspNetCore.Mvc.DataAnnotations/Internal/FileExtensionsAttributeAdapter.cs
@@ -0,0 +1,48 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
+
+namespace Microsoft.AspNetCore.Mvc.DataAnnotations.Internal
+{
+    public class FileExtensionsAttributeAdapter : AttributeAdapterBase<FileExtensionsAttribute>
+    {
+        private readonly string _extensions;
+        
+        public FileExtensionsAttributeAdapter(FileExtensionsAttribute attribute, IStringLocalizer stringLocalizer)
+            : base(attribute, stringLocalizer)
+        {
+            _extensions = Attribute.Extensions.ToString(CultureInfo.InvariantCulture);
+        }
+
+        /// <inheritdoc />
+        public override void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-file-extensions", GetErrorMessage(context));
+        }
+
+        /// <inheritdoc />
+        public override string GetErrorMessage(ModelValidationContextBase validationContext)
+        {
+            if (validationContext == null)
+            {
+                throw new ArgumentNullException(nameof(validationContext));
+            }
+
+            return GetErrorMessage(
+                validationContext.ModelMetadata,
+                validationContext.ModelMetadata.GetDisplayName(),
+                Attribute.Extensions);
+        }
+    }
+}
